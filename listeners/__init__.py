0ba9d8b8f2ef9c0ad29708b37c2e00eda2e75622
Michael Hurhangee <m.hurhangee@me.com>
2025-05-09 10:06:34 +0000

    feat: reply in thread

--- listeners/__init__.py
+++ listeners/__init__.py
@@ -1,5 +1,5 @@
 import logging
-from .assistant import assistant, respond_to_mention
+from .assistant import assistant, respond_to_mention, respond_to_thread_message
 from .commands import echo_command
 from .home_tab import home_opened
 
@@ -21,6 +21,10 @@ def register_listeners(app):
     # Register the app_mention event handler
     logger.debug("Registering app_mention event handler")
     app.event("app_mention")(respond_to_mention)
+    
+    # Register the message event handler for thread messages
+    logger.debug("Registering message event handler for thread messages")
+    app.event("message")(respond_to_thread_message)
 
     # Register the assistant middleware
     logger.debug("Registering assistant middleware")
